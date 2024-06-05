22e7f180933b8ac661ffa2a8ac7c39a77ee22694
Deepak <kumardk8780@gmail.com>
2024-06-05 12:42:34 +0530

    removed commented code

--- src/examples/MenuExample/index.stories.tsx
+++ src/examples/MenuExample/index.stories.tsx
@@ -2,7 +2,7 @@ import { Meta, StoryObj } from "@storybook/react";
 import { ContextMenuItem } from ".";
 
 const meta: Meta<typeof ContextMenuItem> = {
-    title: "Examples/Context Menu",
+    title: "Examples/Menu",
     component: ContextMenuItem,
     parameters: {
         layout: "centered",
@@ -13,68 +13,4 @@ export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-export const Menu: Story = {};
-
-// import { MenuItem } from "@/components/ContextMenu/MenuItem";
-// import { LeftIcon, RightIcon } from "@/components/ContextMenu/MenuItem/Icon/icon";
-// import { Meta, StoryObj } from "@storybook/react";
-// import { Menu } from ".";
-
-
-// const meta: Meta<typeof Menu> = {
-//     title: "Examples/Menu",
-//     component: Menu,
-//     parameters: {
-//         layout: "centered",
-//     },
-// };
-
-// export default meta;
-
-// type Story = StoryObj<typeof meta>;
-
-// export const MenuItems: Story = {
-//     args: {
-//         children: (
-//             <>
-//                 <MenuItem
-//                     label="Print packing slip"
-//                     size="md"
-//                     className="bg-inherit items-center justify-center border shadow"
-//                 />
-//                 <MenuItem
-//                     label="View return/refund status"
-//                     size="md"
-//                     className="bg-inherit items-center justify-center border shadow"
-
-//                 />
-
-//                 <MenuItem
-//                     label="Leave seller feedback"
-//                     size="md"
-//                     className="bg-inherit items-center justify-center border shadow"
-
-
-//                 />
-
-//                 <MenuItem
-//                     label="Leave delivery feedback"
-//                     size="md"
-//                     className="bg-inherit items-center justify-center border shadow"
-
-
-//                 />
-
-//                 <MenuItem
-//                     label="Write product review"
-//                     size="md"
-//                     className="bg-inherit items-center justify-center border shadow"
-
-
-//                 />
-//             </>
-
-//         ),
-//         label: "View return label and instructions"
-//     }
-// }
+export const View: Story = {};

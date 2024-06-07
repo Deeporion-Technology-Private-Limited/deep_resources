79ec42d62511c84993cdec1acfb22e143c37c92f
Naresh Sharma <naresh@ampai.ai>
2024-06-07 13:28:48 +0530

    Add rounded corners

---
+++ src/components/RoundedCorners/type.tsx
@@ -0,0 +1,12 @@
+export enum BorderRadius {
+  None = "rounded-none",
+  Small = "rounded-sm",
+  Medium = "rounded",
+  MediumMD = "rounded-md",
+  Large = "rounded-lg",
+  XL = "rounded-xl",
+  DoubleXL = "rounded-16px",
+  TripleXL = "rounded-24px",
+  QuadrupleXL = "rounded-32px",
+  Full = "rounded-full"
+}

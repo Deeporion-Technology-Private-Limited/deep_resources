1b23a4b71bd591074dc27ec485d8b23cb5b79282
MitaliDeep17 <mitali.khandelwal@deeporion.com>
2024-06-10 18:03:16 +0530

    rever modal changes

--- src/examples/LoginForm/LoginForm.tsx
+++ src/examples/LoginForm/LoginForm.tsx
@@ -1,72 +1,114 @@
-import { Box, Button, Input, Stack, Text } from "@/components";
-import { ButtonVariant } from "@/components/Button/type";
+import { Box, Button, IconButton, Input, Stack, Text } from "@/components";
+import { ButtonDirection, ButtonVariant } from "@/components/Button/type";
+import background from "../../images/backimage.png";
+import apple from "../../images/apple.png";
+import google from "../../images/google.png";
+import facebook from "../../images/facebook.png";
 
 export const LoginForm = () => {
   return (
-    <Box className="px-8 py-12 border border-gray-300 rounded-xl">
-      <Stack>
-        <Text
-          as="h2"
-          weight={"bold"}
-          align={"center"}
-          size={"3xl"}
-          className="mb-2"
-        >
-          Login
-        </Text>
+    <Box className=" w-[100vw] flex items-center justify-between">
+      <Box className="w-[50vw] flex justify-center flex-col items-center">
+      <Text as="label" weight={"black"}  className="absolute left-7 top-5 text-2xl"> 
+            Logo
+          </Text>
+        <Box className="w-fit">
+          
+          <Stack className="relative">
+            <Text as="h2" weight={"bold"} size={"3xl"} className="mb-9" >
+              Login
+            </Text>
 
-        <Text
-          as="span"
-          emphasis={"low"}
-          size={"sm"}
-          align={"center"}
-          className="mb-8"
-        >
-          Please enter your credentials to login
-        </Text>
+            <Text
+              as="label"
+              htmlFor="username"
+              size={"sm"}
+              weight={"medium"}
+              className="mb-1.5"
+            >
+              Username
+            </Text>
+            <Input
+              type="text"
+              id="username"
+              placeholder="Username"
+              className="mb-4"
+              style={webstyle.inputBoxDesign}
+            />
 
-        <Text
-          as="label"
-          htmlFor="username"
-          size={"sm"}
-          weight={"medium"}
-          className="mb-1.5"
-        >
-          Username
-        </Text>
-        <Input
-          type="text"
-          id="username"
-          placeholder="Username"
-          className="mb-4"
-          style={webstyle.inputBoxDesign}
-        />
+<Box>
 
-        <Text
-          as="label"
-          htmlFor="password"
-          size={"sm"}
-          weight={"medium"}
-          className="mb-1.5"
-        >
-          Password
-        </Text>
-        <Input 
-          id="password" 
-          type="password" 
-          placeholder={"Password"} 
-          style={webstyle.inputBoxDesign}
-        />
+            <Text
+              as="label"
+              htmlFor="password"
+              size={"sm"}
+              weight={"medium"}
+              className="mb-1.5"
+            >
+              Password
+            </Text>
+            <Input
+              id="password"
+              type="password"
+              placeholder={"Password"}
+              style={webstyle.inputBoxDesign}
+            />
+            <Text
+              as="label"
+              htmlFor="password"
+              size={"sm"}
+              weight={"bold"}
+              className="mb-1.5 w-full  text-end block text-[#7D5D4F]"
 
-        <Button
-          type="submit"
-          variant={ButtonVariant.DefaultPrimary}
-          className="mt-10"
-          style={webstyle.loginButton}
-        >
-          Login
-        </Button>
-      </Stack>
+            >
+              Forgot Password
+            </Text>
+</Box>
+
+            <Button
+              type="submit"
+              variant={ButtonVariant.DefaultPrimary}
+              className="my-10"
+              style={webstyle.loginButton}
+            >
+              Login
+            </Button>
+
+            <Box className="flex w-[100%] justify-center ">
+              <IconButton
+                iconUrl={google}
+                backgroundColor="transparent"
+                direction={ButtonDirection.Row}
+              />
+              <IconButton
+                iconUrl={apple}
+                backgroundColor="transparent"
+                direction={ButtonDirection.Row}
+              />
+              <IconButton
+                iconUrl={facebook}
+                backgroundColor="transparent"
+                direction={ButtonDirection.Row}
+              />
+            </Box>
+
+            <Text as="span" emphasis={"low"} size={"sm"} align={"center"}>
+              Already have an account?
+              <Text as="label" weight={"medium"}
+              className="text-black ml-2"
+      >
+                Register
+              </Text>
+            </Text>
+          </Stack>
+        </Box>
+      </Box>
+      <Box className="relative">
+        <img src={background} alt=""  className=""/>
+        <Text className="absolute bottom-28 left-56 w-96 text-center text-white text-3xl"  weight={"bold"}>
+        Serve yourself some delicious cuisine
+        </Text>
+      </Box>
     </Box>
   );
 };
@@ -92,5 +134,9 @@ const webstyle = {
     color: "#FFFFFF",
     lineHeight: "24px",
     align: "center",
+  },
+  registertext: {
+    color: "black",
+    marginLeft: "0.5rem",
   },
 };

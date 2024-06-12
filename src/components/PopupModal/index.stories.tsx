c43eedfd8a9bb9b3f9c120c2d7d321f3ab5811e0
VeereshHerurmath1996 <veeresh.herurmath@deeporion.com>
2024-06-12 17:01:42 +0530

    Review forms added in modal

--- src/components/PopupModal/index.stories.tsx
+++ src/components/PopupModal/index.stories.tsx
@@ -1,5 +1,10 @@
 import type { Meta, StoryObj } from "@storybook/react";
-import  {Modal}  from ".";
+import { Modal } from ".";
+import { Checkbox, Headings, Input, Text } from "..";
+import { InputType } from "../Input/type";
+import ReviewCard from "../Cards/ReviewCard/ReviewCard";
+import { LoginForm } from "../../examples/LoginForm/LoginForm";
+
 const meta = {
   title: "Components/PopupModal/Modal",
   component: Modal,
@@ -10,30 +15,153 @@ const meta = {
     // contentSize: {
     //   control:'select', "options" :["text-xl","text-2xl","text-3xl","text-4xl","text-5xl"],
     // },
-   
   },
   tags: ["autodocs"],
-} satisfies Meta<typeof Modal> ;
-
+} satisfies Meta<typeof Modal>;
 
 export default meta;
 
 type Story = StoryObj<typeof meta>;
 
+export const Default: Story = {
+  args: {
+    SaveChanges: () => console.log("saved"),
 
-export const Default :Story = {
-args : {
-  SaveChanges: () => console.log("saved"),
-  
-  onClose: () =>(isOpen: boolean) => { return !isOpen; },
-  title: 'Sample Modal',
-  contents:  "Contents in the modals ",
-  button02:true,
-  button01:true,
-  buttontext1:"button01",
-  buttontext2:"button02",
+    onClose: () => (isOpen: boolean) => {
+      return !isOpen;
+    },
+    title: "Sample Modal",
+    contents: "Contents in the modals ",
+    button02: true,
+    button01: true,
+    buttontext1: "Text 1",
+    buttontext2: "Text 2",
+    children: (
+      <div>
+        <Headings
+          FontSize={"text-base"}
+          fontWeight="font-normal"
+          text={"Contents in the modals"}
+        ></Headings>
+      </div>
+    ),
+  },
+};
 
-  
-  
-}
+export const ModalInputs: Story = {
+  args: {
+    SaveChanges: () => console.log("saved"),
+
+    onClose: () => (isOpen: boolean) => {
+      return !isOpen;
+    },
+    title: "Sample Modal",
+    contents: "Contents in the modals ",
+    button02: true,
+    button01: true,
+    buttontext1: "button01",
+    buttontext2: "button02",
+    children: (
+      <>
+        <Headings
+          className="font-normal "
+          FontSize={"text-base"}
+          fontWeight="font-normal"
+          text={"heading in the modals"}
+        ></Headings>
+
+        <div className="flex flex-col mb-4">
+          <label
+            htmlFor="input1"
+            className="mb-2 text-lg font-normal text-slate-500 "
+          >
+            Name :
+          </label>
+          <Input id="input1" placeholder="Input 1" type={InputType.Text} />
+        </div>
+        <div className="flex flex-col mb-4">
+          <label
+            htmlFor="input2"
+            className="mb-2 text-lg font-normal text-slate-500"
+          >
+            Email :
+          </label>
+          <Input id="input1" placeholder="Input 1" type={InputType.Text} />
+        </div>
+      </>
+    ),
+  },
+};
+
+export const ReviewModal: Story = {
+  args: {
+    SaveChanges: () => console.log("saved"),
+
+    onClose: () => (isOpen: boolean) => {
+      return !isOpen;
+    },
+    title: "Sample Modal",
+    contents: "Contents in the modals ",
+    button02: true,
+    button01: true,
+    buttontext1: "button01",
+    buttontext2: "button02",
+    children: (
+      <>
+        <Headings
+          className="font-normal "
+          FontSize={"text-base"}
+          fontWeight="font-normal"
+          text={"Rate your experience"}
+        ></Headings>
+
+        <ReviewCard
+          style={{ boxShadow: "none" }}
+          imageSrc={""}
+          rating={4}
+          className="shadow-none"
+          StarRating
+        />
+      </>
+    ),
+  },
+};
+
+export const PivacyPolicy: Story = {
+  args: {
+    SaveChanges: () => console.log("saved"),
+
+    onClose: () => (isOpen: boolean) => {
+      return !isOpen;
+    },
+    title: "Terms and Conditions",
+    contents: " ",
+    button02: true,
+    button01: true,
+    buttontext1: "button01",
+    buttontext2: "button02",
+    children: (
+      <>
+        <Text className="mb-4">"Contents in the Terms and conditions"</Text>
+
+        <Checkbox label="Accept the Terms and Conditions" type="checkbox" />
+      </>
+    ),
+  },
+};
+export const LoginForms: Story = {
+  args: {
+    SaveChanges: () => console.log("saved"),
+
+    onClose: () => (isOpen: boolean) => {
+      return !isOpen;
+    },
+    title: "Login Forms",
+    contents: "Contents in the modals ",
+    button02: false,
+    button01: false,
+    buttontext1: "button01",
+    buttontext2: "button02",
+    children: <LoginForm />,
+  },
 };

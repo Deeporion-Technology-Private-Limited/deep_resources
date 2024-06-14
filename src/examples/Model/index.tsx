4abe175e3ad4193e52eb8443e39f51b1a2717985
VeereshHerurmath1996 <veeresh.herurmath@deeporion.com>
2024-06-14 17:37:21 +0530

    Size chart modal implemented

--- src/examples/Model/index.tsx
+++ src/examples/Model/index.tsx
@@ -1,13 +1,19 @@
-import { Box, Headings, Menu, MenuItem, Modal } from "@/components";
+import { Box,Button, Headings, Menu, MenuItem, Modal } from "@/components";
+import { ButtonIconPosition,ButtonSize,ButtonVariant } from "@/components/Button/type";
 import ProductCard from "@/components/Cards/ProductCards/Product_card";
 // import { ProductCard } from "@/components/ProductCard";
+import chart from "./Icons/chart.svg"
+import hearticon from "./Icons/hearticon.svg"
 import { TableHeader } from "@/components/Table/TableHeader";
 
 export const Sizechartmodel = () => {
+  
   return (
     <Modal
-      button01
-      button02
+      button01 = {false}
+      button02 = {false}
+          modalbutton={true}
+          onClose={() => { } } openModal={false}      
       header={<Box className="mb-3 flex   border-[rgba(255, 255, 255, 1)]">
         <Box>
           <ProductCard
@@ -20,9 +26,11 @@ export const Sizechartmodel = () => {
           <Headings fontWeight={"font-bold"} FontSize="text-base" text="Mango" />{" "}
         </Box>
       </Box>}
-      modalbutton={true}
-      onClose={() => { } } openModal={false}      
          >
+          <Box className="flex justify-center mb-3">
+            <button className="w-3/6 border-b pb-2 pt-2 hover:border-b-[#3F271E] hover:font-bold "> Size Chart  </button>
+            <button className="w-3/6 border-b hover:border-b-[#3F271E] hover:font-bold">How to Measure</button>
+          </Box>
       <TableHeader
         data={[
           ["XS", "35.0", "24.0", "13.0"],
@@ -39,6 +47,29 @@ export const Sizechartmodel = () => {
         ]}
         variant="solid"
       />
+<Box className="flex mt-3">
+<Button
+  hover
+  iconPosition={ButtonIconPosition.Left}
+  iconUrl={chart}
+  size={ButtonSize.Small}
+  variant={ButtonVariant.DefaultPrimary}
+  className="mr-1.5"
+>
+Add to Cart
+</Button>
+<Button
+  hover
+  iconPosition={ButtonIconPosition.Left}
+  iconUrl={hearticon}
+  size={ButtonSize.Small}
+  variant={ButtonVariant.DefaultPrimary}
+  className="ml-1.5"
+>
+Wishlist
+</Button>
+</Box>
+
     </Modal>
   );
 };

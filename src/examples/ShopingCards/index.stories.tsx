fef44e69e4c73d7ce2997be385a124a49df67279
chatnayjangid <chatany.jangid@deeporion.com>
2024-06-18 19:12:10 +0530

    watch lIst created

--- src/examples/ShopingCards/index.stories.tsx
+++ src/examples/ShopingCards/index.stories.tsx
@@ -1,13 +1,24 @@
 import { Meta, StoryObj } from "@storybook/react";
 import Cards from ".";
-
+import { demo ,array} from "./data";
 const meta: Meta<typeof Cards> = {
     title: "Examples/ShopingCards",
     component: Cards,
+    tags:['autodocs']
 }
 
 export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-export const View: Story = {};
+export const View: Story = {
+    args:{
+        item:demo
+    }
+};
+
+export const WatchList: Story = {
+    args:{
+        item:array
+    }
+};

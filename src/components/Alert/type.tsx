2bc99e92b564fbb2f7189340b1da6856d417d5dd
Priyal Jain <priyal.jain@deeporion.com>
2024-06-19 10:50:43 +0530

    Alert component added

---
+++ src/components/Alert/type.tsx
@@ -0,0 +1,11 @@
+export enum AlertType {
+  Success = "success",
+  Info = "info",
+  Warning = "warning",
+  Error = "error",
+}
+
+export enum AlertVariant {
+  Filled = "filled",
+  Outlined = "outlined",
+}

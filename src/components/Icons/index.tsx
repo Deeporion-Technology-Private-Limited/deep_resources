f57480d116409f434206d34ca046171d4d2f0eb6
niteshMeena1 <nitesh.meena@deeporion.com>
2024-06-28 16:35:19 +0530

    Created Admin SubscriptionPage

--- src/components/Icons/index.tsx
+++ src/components/Icons/index.tsx
@@ -173,3 +173,13 @@ fill="#DC2626"/>
         </div>
     )
 }
+
+export const ThreeDostIcon = ()=>{
+    return(
+        <div>
+            <svg width="30px" height="10px" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" fill="#000000">
+  <path d="M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z"/>
+</svg>
+        </div>
+    )
+}

e443f215e5637934c9dda7be47047612b0d9662b
Karanbairwaa12 <karanbairwaa@gmail.com>
2024-07-02 13:41:24 +0530

    add content to admin bar

--- src/examples/AdminSidebar/index.stories.tsx
+++ src/examples/AdminSidebar/index.stories.tsx
@@ -1,10 +1,15 @@
 import { Meta, StoryObj } from "@storybook/react";
 import { AdminSidebar } from ".";
+import log from "./icon/Logo.svg";
+import { Data } from "./adminbarConst";
 
 const meta: Meta<typeof AdminSidebar> = {
   title: "Examples/AdminSidebar",
   component:AdminSidebar,
   tags: ["autodocs"],
+  parameters: {
+    layout: "centered",
+  }
 };
 
 export default meta;
@@ -12,5 +17,8 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
-
+  args: {
+    Data: Data,
+    logo: log
+  }
 };

949501abba4de9888660704a02b1ddb561149471
Deepak <kumardk8780@gmail.com>
2024-07-02 19:07:10 +0530

    fixed tsc issue

--- src/examples/AddNewGiftCards/index.tsx
+++ src/examples/AddNewGiftCards/index.tsx
@@ -1,9 +1,9 @@
 import { Box, Button, Input, Text } from "@/components";
-import SearchImage from "@/components/Input/Icons/SearchImage";
 import { cn } from "@/utils";
 import { ChangeEvent, ComponentProps, DragEventHandler, forwardRef, useRef, useState } from "react";
 import { ILabels } from "../GiftCards/data";
 import { InputType, InputVariant } from "@/components/Input/type";
+import SearchImage from "@/images/InputIcons/SearchImage";
 
 type AddNewGiftCardProps = ComponentProps<typeof Box> & {
     labelsArray: ILabels[];

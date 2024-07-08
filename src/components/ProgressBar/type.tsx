e38829f9bc13ec7f35514f455f37fe44b4cb2501
Rajshekhar-policepatil <raj.shekar@deeporion.com>
2024-07-08 12:50:29 +0530

    Test On Alert

--- src/components/ProgressBar/type.tsx
+++ src/components/ProgressBar/type.tsx
@@ -1,16 +1,5 @@
 export enum variants {
-  circular = "circular",
-  linear = "linear",
-}
-
-export interface IProgressBar {
-  progress: number;
-  variant?: string;
-  size?: number;
-  strokeWidth?: number;
-  bgColor: string;
-  textFont?: string;
-  textColor?: string;
-  rotate?: string;
-  middleText?: boolean;
-}
+    circular = "circular",
+    linear="linear",
+  }
+  
---
+++ src/components/SingleSpliceChart/_test/SingleSpliceChart.test.tsx
@@ -0,0 +1,72 @@
+import { render } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import { SingleLineChart } from "../index";
+
+const XAxisVal = [10, 20, 30, 40];
+const yAxisLabels = ["0", "10", "20", "30", "40", "50"];
+const XAxisLab = ["Jan", "Feb", "Mar", "Apr"];
+
+describe("SingleLineChart component", () => {
+  // test("renders with default props", () => {
+  //   const { getByTestId } = render(<SingleLineChart />);
+  //   expect(getByTestId("place")).toBeInTheDocument();
+  // });
+
+  test("updates on props change", () => {
+    const { rerender, getByText } = render(
+      <SingleLineChart xAxisValues={XAxisVal} xAxisLabels={XAxisLab} />
+    );
+
+    rerender(<SingleLineChart xAxisValues={XAxisVal} xAxisLabels={XAxisLab} />);
+    expect(getByText("Mar")).toBeInTheDocument();
+  });
+
+  test("renders correct SVG path", () => {
+    const { getByTestId } = render(
+      <SingleLineChart xAxisValues={XAxisVal} xAxisLabels={XAxisLab} />
+    );
+
+    expect(getByTestId("graph")).toBeInTheDocument();
+  });
+
+  test("renders horizontal grid lines based on yAxisLabels", () => {
+    const { getByTestId } = render(
+      <SingleLineChart
+        yAxisLabels={yAxisLabels}
+        xAxisValues={XAxisVal}
+        xAxisLabels={XAxisLab}
+      />
+    );
+
+    expect(getByTestId("graph").querySelector("g")).toBeInTheDocument();
+    const lines = getByTestId("graph")
+      .querySelector("g")
+      ?.querySelectorAll("line");
+
+    expect(lines?.length).toBe(yAxisLabels.length - 2);
+
+    const height = 170;
+    lines?.forEach((line, index) => {
+      const expectedY =
+        height - ((index + 1) / (yAxisLabels.length - 1)) * height;
+      expect(line).toHaveAttribute("x1", "0");
+      expect(line).toHaveAttribute("x2", "360"); // Assuming width = xAxisLabels.length * 90
+      expect(line).toHaveAttribute("y1", expectedY.toString());
+      expect(line).toHaveAttribute("y2", expectedY.toString());
+    });
+  });
+
+  test("renders y-axis labels correctly", () => {
+    const { getByText } = render(
+      <SingleLineChart
+        yAxisLabels={yAxisLabels}
+        xAxisValues={XAxisVal}
+        xAxisLabels={XAxisLab}
+      />
+    );
+
+    yAxisLabels.forEach((label) => {
+      expect(getByText(label)).toBeInTheDocument();
+    });
+  });
+});
